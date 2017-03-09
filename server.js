52d39e8ef1f4e6578383c44955798fd3407efc56
Paris Do <pcdo@ucsd.edu>
2017-03-09 12:19:21 -0800

    hello

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var strategy = {
 
 // Database Connection
 var db = mongoose.connection;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/cogs121');
+mongoose.connect(process.env.MONGOLAB_BLACK_URI || 'mongodb://127.0.0.1/cogs121');
 db.on('error', console.error.bind(console, 'Mongo DB Connection Error:'));
 db.once('open', function(callback) {
     console.log("Database connected successfully.");
